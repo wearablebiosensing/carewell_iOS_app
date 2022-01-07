b0fe0f70936ec71741bcf98ec4348f2751fb5ab2
shehjarsadhu <shehjar_sadhu@my.uri.edu>
2022-01-07 15:01:50 -0500

    Web view omplementation

--- carewellapp/lib/navigation_elements/education.dart
+++ carewellapp/lib/navigation_elements/education.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class education extends StatelessWidget{
   @override
   Widget build(BuildContext contex){
-    return Container(
-        child:Center(child: Text("Education"))
+    return WebView(
+      initialUrl: "https://sites.google.com/view/carewell/home",//"https://sites.google.com/uri.edu/care-well-education-ipad2/home",
+      javascriptMode: JavascriptMode.unrestricted,
     );
-
-
+    //   Container(
+    //     child:Center(child: Text("Education"))
+    // );
   }
-
 }
--- carewellapp/lib/navigation_elements/managing_care.dart
+++ carewellapp/lib/navigation_elements/managing_care.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class managing_care extends StatelessWidget{
   @override
   Widget build(BuildContext contex){
-    return Container(
-        child:Center(child: Text("Managing Care"))
+    return WebView(
+      initialUrl: "https://sites.google.com/view/managingcare-cw/home",//"https://sites.google.com/uri.edu/care-well-education-ipad2/home",
+      javascriptMode: JavascriptMode.unrestricted,
     );
 
-
+    //
   }
 
 }
